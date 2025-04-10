bb4562da01a8c0279cdd25977c11813ca97fd37d
Dylan Lu <dylanelu@gmail.com>
2025-04-10 15:00:21 -0700

    screen capture

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from geometry-dash-ai!")
+import time
+
+import cv2
+import numpy as np
+
+import mss
+
+# x start 128, y 36
+# x end is 1385, y 980
+
+with mss.mss() as sct:
+    # Part of the screen to capture
+    monitor = {
+        "top": 40,
+        "left": 128,
+        "width": 800,
+        "height": 600
+    }
 
 
-if __name__ == "__main__":
-    main()
+    while "Screen capturing":
+        last_time = time.time()
+
+        # Get raw pixels from the screen, save it to a Numpy array
+        img = np.array(sct.grab(monitor))
+
+        # Display the picture
+        cv2.imshow("OpenCV/Numpy normal", img)
+
+        # Display the picture in grayscale
+        # cv2.imshow('OpenCV/Numpy grayscale',
+        #            cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY))
+
+        print(f"fps: {1 / (time.time() - last_time)}")
+
+        # Press "q" to quit
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
