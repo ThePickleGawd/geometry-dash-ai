67e312ee7b864bdfc0bb940b9011ab3938608f81
Ishan Katpally <ishankatpally@cs.ucsb.edu>
2025-04-26 15:49:13 -0700

    freezing game works wowowowow

--- gd-rl-mod/src/main.cpp
+++ gd-rl-mod/src/main.cpp
@@ -7,22 +7,8 @@ using namespace geode::prelude;
 class $modify(MyPlayerObject, PlayerObject) {
 	bool pushButton(PlayerButton btn) {
 		log::info("Jump button received by player!");
+		// PlayerObject::lockPlayer();
+		this->m_isLocked = !this->m_isLocked;
 		return PlayerObject::pushButton(btn);
-	}
-};
-
-
-class $modify(MyPlayLayer, PlayLayer) {
-	bool init(GJGameLevel* level, bool p1, bool p2) {
-		log::info("Level started");
-		return PlayLayer::init(level, p1, p2);
-	}
-	
-	void destroyPlayer(PlayerObject *player, GameObject *p1) {
-		PlayLayer::destroyPlayer(player, p1);
-
-		if (!player->m_isDead) return;
-
-		log::info("player died at {} percent", (m_player1->getPositionX() / m_levelLength)*100.0f); // there's probably a field for this but idk what it is
 	}
 };
